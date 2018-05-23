75c55813a97122f9d7c0be0ce0041fca4f79644d
ivan koop <ivankoop3@gmail.com>
2018-05-23 14:58:33 -0400

    needed messages stored and some code cleaning

--- scraper.py
+++ scraper.py
@@ -6,49 +6,43 @@ from apiclient import errors
 import requests
 import base64
 import email
+import datetime
+
+class MailItem():
+
+    def __init__(self, id, message):
+        self.id = id
+        self.message = message
+
+def getMessages(service, user_id, query= ''):
+    response = service.users().messages().list(userId=user_id,q=query).execute()
+    messages = []
+
+    for message in response['messages']:
+
+        response = service.users().messages().get(userId=user_id, id=message['id'],format='raw').execute()
+        msg_str = base64.urlsafe_b64decode(response['raw'].encode('ASCII'))
+        messages.append(MailItem(message['id'],msg_str))
+
+    return messages
+
+
+def messagesFilter(stored_messages):
+
+    for messages in stored_messages:
+        print(messages.id)
+
 
 
 SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
 store = file.Storage('credentials.json')
 creds = store.get()
+
 if not creds or creds.invalid:
     flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
     creds = tools.run_flow(flow, store)
+
 service = build('gmail', 'v1', http=creds.authorize(Http()))
-
-
-query = 'webmaster@tedepasa.com'
-user_id = 'me'
-
-
-def getMessagesid(service, user_id, query= ''):
-    response = service.users().messages().list(userId=user_id,q='').execute()
-    messages = []
-
-    print(query)
-
-    if 'messages' in response:
-        messages.extend(response['messages'])
-
-    while 'nextPageToken' in response:
-        page_token = response['nextPageToken']
-        response = service.users().messages().list(userId=user_id, q=query,pageToken=page_token, includeSpamTrash=1, maxResults=2000).execute()
-        messages.extend(response['messages'])
-
-        return messages
-
-print(getMessagesid(service,user_id,query))
-
-
-def getMessage(service,user_id,msg_id):
-
-    message = service.users().messages().get(userId=user_id, id=msg_id,
-                                             format='raw').execute()
-
-    msg_str = base64.urlsafe_b64decode(message['raw'].encode('ASCII'))
-
-    print(msg_str)
-
-    #mime_msg = email.message_from_string(msg_str)
-
-getMessage(service,user_id,'1634c807066c194d')
+current_date = datetime.datetime.today().strftime('%Y-%m-%d')
+stored_messages = getMessages(service,'me','from:webmaster@tedepasa.com after:' + current_date)
+messagesFilter(stored_messages)
